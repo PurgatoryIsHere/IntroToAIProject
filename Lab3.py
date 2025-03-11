fc48c6503679fec2c182b5bc493a634e20a99504
cbgalileo <cbrice@plu.edu>
2025-03-11 16:58:17 -0700

    Update Lab3.py

--- Lab3.py
+++ Lab3.py
@@ -17,11 +17,23 @@ def splitData(data_frame):
     testing = data_frame.tail(math.floor(rows * 0.2))
     return (training, testing)
 
-def splitDataRandom(self):
-    pass
+def splitDataRandom(data_frame):
+
+    data_frame = data_frame.sample(frac=1, random_state=42).reset_index(drop=True)
+    
+    rows = len(data_frame)
+    
+    training = data_frame.head(math.floor(rows * 0.8))
+    testing = data_frame.tail(rows - math.floor(rows * 0.8))
+    
+    return training, testing
 
 
 if __name__ == "__main__":
     training, testing = splitData(jp_stock_frame)
     print(training)
     print(testing)
+
+    randTrain, randTest = splitDataRandom(jp_stock_frame)
+    print(randTrain)
+    print(randTest)
