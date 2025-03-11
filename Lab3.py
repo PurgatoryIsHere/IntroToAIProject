c47ae4b38809db9cbde7f0e35e434e306fb55fff
EArcher <EdgeNaut@gmail.com>
2025-03-11 16:08:33 -0700

    Added in short outline to Lab3 file

---
+++ Lab3.py
@@ -0,0 +1,8 @@
+#import argparse
+#parser = argparse.ArgumentParser()
+#parser.add_argument("--file", required=True, help="Path to dataset file")
+#args = parser.parse_args()
+
+def splitData(self):
+
+def splitDataRandom(self):
