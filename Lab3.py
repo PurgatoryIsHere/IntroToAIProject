58474a747c7d761dc89ef4f438ef3ca909d24a55
EArcher <EdgeNaut@gmail.com>
2025-03-11 16:41:28 -0700

    Quick reader and Data import

--- Lab3.py
+++ Lab3.py
@@ -3,6 +3,16 @@
 #parser.add_argument("--file", required=True, help="Path to dataset file")
 #args = parser.parse_args()
 
+import csv
+
+with open('JPStockPredict.csv', 'r') as file:
+    csvreader = csv.reader(file)
+    header = next(csvreader)
+    print("Header:", header)
+    for row in csvreader:
+        print("Row:", row)
+        # Process data here
+
 def splitData(self):
 
 def splitDataRandom(self):
