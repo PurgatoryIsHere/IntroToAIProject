a144bc6c0847a9e31ef440c121f501bdf50349f2
Guuzzeji <5lh8dl440f@gmail.com>
2025-03-11 16:52:15 -0700

    working splitData

--- Lab3.py
+++ Lab3.py
@@ -4,13 +4,24 @@
 #args = parser.parse_args()
 
 import pandas as pd
+import math
 
 jp_stock_frame = pd.read_csv("JPStockPredict.csv")
 
-print(jp_stock_frame)
+# print(jp_stock_frame)
 
-def splitData(self):
-    pass
+def splitData(data_frame):
+    #  80% training, 20% testing.
+    rows = len(data_frame)
+    training = data_frame.head(math.floor(rows * 0.8))
+    testing = data_frame.tail(math.floor(rows * 0.2))
+    return (training, testing)
 
 def splitDataRandom(self):
     pass
+
+
+if __name__ == "__main__":
+    training, testing = splitData(jp_stock_frame)
+    print(training)
+    print(testing)
